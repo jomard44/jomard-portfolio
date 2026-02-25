eecc566644d4a5bfdec936cecc812185f570cc52
jomard <jomrdali44@gmail.com>
2026-02-25 14:32:51 -0400

    create about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Skills from './components/Skilles'
-import Contact from './components/Contact'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skilles";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-     <Hero/>
-     <Projects/>
-     <Skills/>
-     <Contact />
+      <Navbar />
+      <Hero />
+
+      <Projects />
+      <About />
+      <Skills />
+      <Contact />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <section className="py-20 px-4 max-w-4xl mx-auto text-center">
+      <h2 className="text-3xl font-bold mb-6">About Me</h2>
+
+      <p className="text-gray-700/70 leading-relaxed">
+        I’m a second-year IT Web Programming student at NSCC with a strong
+        interest in full-stack development. I enjoy building real-world
+        applications and improving my problem-solving skills.
+        <br /><br />
+        Recently, I’ve been working on projects like a job tracking system and a
+        gym management app, where I focus on building clean APIs and connecting
+        frontend and backend effectively.
+        <br /><br />
+        I’m currently looking for an opportunity where I can
+        contribute and grow as a developer.
+      </p>
+    </section>
+  );
+};
+
+export default About;
